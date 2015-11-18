f35bed86cd68b93b621bb5f28039800db43bb511
Greg Tarsa <gltarsa@gmail.com>
2015-11-18 13:00:38 -0500

    Clean up elegant code using hash map

--- roman_numerals2.rb
+++ roman_numerals2.rb
@@ -4,16 +4,26 @@ class Fixnum
   def to_roman
     r = ''
     n = self
-    decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+    map = { 'M'  => 1000,
+            'CM' => 900,
+            'D'  => 500,
+            'CD' => 400,
+            'C'  => 100,
+            'XC' => 90,
+            'L'  => 50,
+            'XL' => 40,
+            'X'  => 10,
+            'IX' => 9,
+            'V'  => 5,
+            'IV' => 4,
+            'I'  => 1 }
 
-    decimals.each_with_index do |decimal, i|
+    map.each_pair do |roman, decimal|
       while n >= decimal do
-        r += roman[i];
+        r += roman;
         n -= decimal;
       end
     end
     return r;
   end
 end
-
